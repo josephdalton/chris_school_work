6be0fb8d59e5ba80f4397e2d44751979bf5e95fa
thefad3 <chrismac@new-host-4.home>
2013-09-13 19:55:30 -0400

    undefined error

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -2,13 +2,13 @@
 //Chris Lynch, 9/12/13 Expressions Wacky
 //How many times I wear lip stick per year
 
-var lipstickType = ["Mac", "Loreal", "NYX"]; /* Lipstick Brands 0,1,2,3 */
+var lipstickType = ["Mac", "Loreal", "NYX"]; /* Lipstick Brands 0,1,2 */
 var daysYear = 365; /* Days in a year */
 var timeMonth = 2; /* How many times per month */
 var monthsYear = 12; /* How many months in a year */
 
  
-console.log("I wear", lipstickType[3], "brand lipstick", monthsYear * timeMonth, "times a year","(",daysYear," days in a year)"); /* Calculate how many times a year I wear lip stick */
+console.log("I wear", lipstickType[2], "brand lipstick", monthsYear * timeMonth, "times a year","(",daysYear," days in a year)"); /* Calculate how many times a year I wear lip stick */
 
 
 
