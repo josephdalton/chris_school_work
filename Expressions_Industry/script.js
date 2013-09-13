e2ff35a0b930849768ffe649a8efc2eb8f166bfc
thefad3 <chrismac@new-host-4.home>
2013-09-13 19:40:00 -0400

    Industry Upload

---
+++ Expressions_Industry/script.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+//Chris Lynch, 9/12/13 Expressions Industry
+//Defining how many energy drinks I drink per year
+
+var ramAmounts = ["1GB", "2GB", "3GB"]; /* drink selections 0,1,2,3 */
+var ramSize = 1024; /* RAM stick size in MB */
+var slotSize = 2048; /* How much RAM per slot */
+var slotAmounts = ["1","2","3"]; /* How many RAM slots */
+
+ 
+console.log("There can be", ramAmounts[0], "with", slotAmounts[2], "avaliable.","/n There are", slotSize * 2, "unallocated RAM"   ); /* Calculate how much RAM is used, and how much is avaliable */
+
+
+
+
+
+
+
+
+
+
+
