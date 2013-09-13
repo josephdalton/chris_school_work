2f871d7faceb0e4934390c125666263af48c1fdd
thefad3 <chrismac@new-host-4.home>
2013-09-13 19:06:18 -0400

    Amount of Energy Drinks per day

---
+++ Expressions_Personal/script.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+//Defining how many energy drink per year
+
+//Define Vars
+
+var energyDrinks; /* Create var for drinks */
+var amountDays; /* Create var for how many days per year */
+var perDay; /* Create var for how many drinks per day */
+
+energyDrinks = 1; /* define the energy drinks*/
+amountDays = 365; /* days per year */
+perDay = 2; /* how many energy drinks per day */
+
+console.log = [amountDays * perDay * energyDrinks]; /* Multiplies amount of days times per day times the amount of energy drinks per purchase 2 x 365 x 1 = 730  */
+
+
+
+
+
+
+
