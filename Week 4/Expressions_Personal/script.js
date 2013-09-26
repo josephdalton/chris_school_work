6cfad471cb7f9bd05f32966ebd47d432b8002dcb
thefad3 <chrismac@new-host-4.home>
2013-09-26 19:44:26 -0400

    Week 4

---
+++ Week 4/Expressions_Personal/script.js
@@ -0,0 +1,16 @@
+// Chris Lynch 9/24/13 Functionals Personal
+
+
+function calcFood(){
+	var teaSpn = 2;
+	var weekDys = 7;
+	var monthDys = 31;
+	var foodWk = teaSpn * weekDys;
+	var foodMon = teaSpn * monthDys;
+	
+	console.log("My ferret eats" + teaSpn + "Tsp. per day. Thats " + foodWk + "Tsp. per week and" + foodMon + "Tsp. per month. ")
+}
+
+
+
+
---
+++ Week 4/Expressions_Wacky/script.js
@@ -0,0 +1,20 @@
+// Chris Lynch 9/19/13 Conditionals Wacky
+
+var jordanName = "Air Foamposite Thermal Maps"
+/*Defining age required to get a CDL*/
+var buyJordan = "225";
+/*Defining my age*/
+var myMoney = "500000";
+
+
+/* if the required age is equal to or greater than */
+if(buyJordan<=myMoney)
+{
+	/*Output the information if the equasion is greater then 21 */
+	console.log("You can buy your", jordanName, "because you have", myMoney, "dollars and they cost", buyJordan, "dollars.. Added to Cart");
+}
+else{
+	/* if the age is not greater than 21 this result will show */
+	console.log("Sorry you cant buy your", jordanName, "because you only have", myMoney, "dollars and they cost", buyJordan, "dollars..");
+	
+	};
