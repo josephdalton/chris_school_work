8bc12b7399e0997807913bdf6ff4b40df9ea0566
thefad3 <chrismac@new-host-4.home>
2013-09-26 21:15:42 -0400

    final

--- Week 4/Expressions_Industry/script.js
+++ Week 4/Expressions_Industry/script.js
@@ -1,15 +1,15 @@
 // Chris Lynch 9/24/13 Functionals Industry
 
 
-var calcPages = function(scriptDay, daysYear, daysMonth){
-	var scriptsYr = scriptDay * daysYear;
-	var scriptsMo = scriptDay * daysMonth;
+var calcPages = function(scriptDay, daysYear, daysMonth){ /* defines the functions using an anonymous function  */
+	var scriptsYr = scriptDay * daysYear; /* multiplying scripts per day times days per year */
+	var scriptsMo = scriptDay * daysMonth; /* multiplying scripts per day for times per month */
 	
-console.log("My job requires me to make " + scriptsMo + " Javascripts a Month. " + "That's " + scriptsYr + " scripts each year!")
+console.log("My job requires me to make " + scriptsMo + " Javascripts a Month. " + "That's " + scriptsYr + " scripts each year!") /* output the informaton and calculations from the stored information and functions  */
 	
 }
 
-calcPages(4, 365, 31);
+calcPages(4, 365, 31); /* calls the function and inputs the numerical data to be stored */
 
 
 
