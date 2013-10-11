abaef63ec45777a5cace0e7c08f8a16f0946cf3f
thefad3 <chrismac@new-host-4.home>
2013-10-10 20:32:55 -0400

    hw

--- Week 2 Expressions/Expressions_Industry/script.js
+++ Week 2 Expressions/Expressions_Industry/script.js
@@ -3,15 +3,15 @@
 //Amount of hard drive space used and left calculation
 
 
-var lenghtTime = ["30", "60", "90"]; /* how many days in a month */
+var lenghtTime = ["30", "60", "90"]; /* how many days in a month 0,1,2 */
 var driveSpace = 500; /* total data on hard drive */
 var spaceDay = 2; /* How many gigabytes per day used */
-var timeLength = lenghtTime[1]; /* creating the time length var */
-var mathVar = lenghtTime * spaceDay; /* multiplying days times amount per day  */
+var timeLength = lenghtTime[0]; /* creating the time length var */
+var mathVar = lenghtTime[0] * spaceDay; /* multiplying days times amount per day  */
 var spaceLeft = driveSpace - mathVar; /* subtract the space used on hard drive  */
 
  
-console.log("You have ", driveSpace,". If you use ", spaceDay, "Gigabytes per day, in", lenghtTime[1], " days you will have ", mathVar , " Gigabytes of data and will have",  mathVar + lenghtTime[1], " in 30 more days. You have ", spaveLeft," Gigabytes of data left."   ); /* exporting the data into the text*/
+console.log("You have", driveSpace," Gigabytes of space. If you use", spaceDay, "Gigabytes per day, in", lenghtTime[0], " days you will have", mathVar , " Gigabytes of data. You have", spaceLeft," Gigabytes of data left."   ); /* exporting the data into the text*/
 
 
 
