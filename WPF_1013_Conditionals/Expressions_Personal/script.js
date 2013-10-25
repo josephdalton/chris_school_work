f0fa1dabc5abaa08a62ac0c2f3f11740723eccbe
josephdalton <jdalton@fullsail.com>
2013-10-25 10:45:02 -0400

    JMD

--- WPF_1013_Conditionals/Expressions_Personal/script.js
+++ WPF_1013_Conditionals/Expressions_Personal/script.js
@@ -3,7 +3,7 @@
 /* The cost of Xbox One */
 var xboxCost = "499";
 /* The total amount of money I have */
-var myMoney = "354";
+var myMoney = "354"; //This Data is being stored as a string not a number
 
 /* using an ternary to see if I have enough money for the xbox one and displaying the results*/
 (myMoney >= xboxCost || xboxCost < myMoney)?(console.log('You have enough money to buy the Xbox One!')):(console.log('Sorry, but you do not have enough money for Xbox One.'));
