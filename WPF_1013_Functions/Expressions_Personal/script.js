934d5a1c0876c8e51a299cd63c35eee0470135bb
thefad3 <chrismac@chriss-mbp.home>
2013-10-24 23:25:23 -0400

    bug with one of the vars being myBank change

--- WPF_1013_Functions/Expressions_Personal/script.js
+++ WPF_1013_Functions/Expressions_Personal/script.js
@@ -8,5 +8,5 @@ var myCredit = "1000";
 var costCloths = "220";
 
 /* using a ternary I use the cash I spend to be greater then or equal to the cost and I make sure that I have enough in my bank just incase I dont have enough cash */
-(costCloths >= moneytoSpend || costCloths >= myBank)?(console.log('You have enough cash to buy your Cloths!')):(console.log('Sorry, but you do not have enough cash to buy your items. You currenty have', moneytospend, ' and ', myCredit, ' On your debit card.'));
+(costCloths >= moneytoSpend || costCloths >= myCredit)?(console.log('You have enough cash to buy your Cloths!')):(console.log('Sorry, but you do not have enough cash to buy your items. You currenty have', moneytospend, ' and ', myCredit, ' On your debit card.'));
 
